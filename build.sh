691af8d0333ca82713d7a5394adc0b1ee6032368
ELLIOTTCABLE <me@ell.io>
2017-06-02 14:29:51 -0400

    + babel --> source-map-support and the new `env` preset

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-babel minimist-shell.es6.js -o minimist-shell.js
-babel bin/minimist-shell.es6.js -o bin/minimist-shell
+babel --source-maps 'inline' minimist-shell.es6.js -o minimist-shell.js
+babel --source-maps 'inline' bin/minimist-shell.es6.js -o bin/minimist-shell
 chmod +x bin/minimist-shell
