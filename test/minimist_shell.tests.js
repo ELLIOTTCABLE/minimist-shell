3ccc650a285eaae4a4508028621a5d6f284d921d
ELLIOTTCABLE <me@ell.io>
2017-06-07 20:03:45 -0500

    + more super-basic testing

--- test/minimist_shell.tests.js
+++ test/minimist_shell.tests.js
@@ -24,8 +24,8 @@ describe("(with `"+minimist.pkg+"`)", function(){
       })
 
       it("doesn't throw, with empty arguments and options", function(){
-         argf        = minimist(['',''], opts = {})
-         opts.shell  = validate_opts(opts)
+         argf         = minimist(['',''], opts = {})
+         opts.shell   = validate_opts(opts)
 
          assert.doesNotThrow(() => flatten_args(argf, opts, opts.shell) )
       })
@@ -37,19 +37,57 @@ describe("(with `"+minimist.pkg+"`)", function(){
       })
 
       it("returns a mapping", function(){
-         argf        = minimist(['',''], opts = {})
-         opts.shell  = validate_opts(opts)
+         argf   = minimist(['',''], opts = {})
 
-         result = flatten_args(argf, opts, opts.shell)
+         result = flatten_args(argf, opts)
          assert(typeof result === 'object')
       })
 
       it("maps simple string-ish flags directly", function(){
-         argf        = minimist(['','', '--foo', 'bar'], opts = {})
-         opts.shell  = validate_opts(opts)
+         argf   = minimist(['','', '--foo', 'bar'], opts = {})
 
-         result = flatten_args(argf, opts, opts.shell)
+         result = flatten_args(argf, opts)
          assert(result.foo === 'bar')
+      })
+
+      it("leaves out undefined options", function(){
+         argf      = minimist(['',''], opts = {})
+         argf.foo  = undefined
+
+         result    = flatten_args(argf, opts)
+         assert(typeof result.foo === 'undefined')
+
+         const flags = Object.keys(result)
+         assert(flags.indexOf('foo') === -1)
+      })
+
+      it("maps numbers to strings in the absence of `opts.typesets`", function(){
+         argf   = minimist(['','', '--foo', '1337'], opts = { number: ['foo'] })
+         assert(typeof argf.foo === 'number')
+
+         result = flatten_args(argf, opts)
+         assert(typeof result.foo === 'string')
+         assert(result.foo === '1337')
+      })
+
+      it("maps boolean-false to the empty string in the absence of `opts.booleans`", function(){
+         argf   = minimist(['','', '--foo', '1337'], opts = { number: ['foo'] })
+         assert(typeof argf.foo === 'number')
+
+         result = flatten_args(argf, opts)
+         assert(typeof result.foo === 'string')
+         assert(result.foo === '1337')
+      })
+
+      it("maps numbers to themselves with `opts.typesets`", function(){
+         opts   = { number: ['foo'], shell: {typesets: true} }
+         argf   = minimist(['','', '--foo', '1337'], opts)
+         assert(opts.shell.typesets === true)
+         assert(typeof argf.foo === 'number')
+
+         result = flatten_args(argf, opts)
+         assert(typeof result.foo === 'number')
+         assert(result.foo === 1337)
       })
 
    })
