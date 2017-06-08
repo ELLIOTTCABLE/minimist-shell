77fbaf8f52059b2c0e6ac813cc79c81e4fb7a557
ELLIOTTCABLE <me@ell.io>
2017-06-08 03:11:25 -0500

    + more flexible errors in the binary

--- bin/minimist-shell.es6.js
+++ bin/minimist-shell.es6.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-const debug           = require('debug')('minimist-shell:bin')
-    , assert          = require('assert')
-    , indent          = require('indent-string')
-    , chalk           = require('chalk')
+const debug              = require('debug')('minimist-shell:bin')
+    , assert             = require('assert')
+    , indent             = require('indent-string')
+    , chalk              = require('chalk')
 
-    , minimist_shell  = require('../minimist-shell.js')
+    , minimist_shell     = require('../minimist-shell.js')
+    , { ArgumentError }  = minimist_shell
+
+
 
 chalk.enabled = true
 
@@ -26,21 +29,28 @@ process.stdin.pipe(require('concat-stream')(function(buffer){ var config, minimi
 
    minimist = config.minimist_package || '_non_existent_package'
    try {
-      minimist = require(minimist) }
+      minimist = require(minimist)
+      debug("using `opts.minimist_package`: '"+minimist+"'") }
    catch (e) { if (e.code !== 'MODULE_NOT_FOUND') throw e
 
+      if (config.minimist_package)
+         debug("`opts.minimist_package` MISSING! '"+minimist+"'")
+
       try {
-         minimist = require('rminimist') }
+         minimist = require('rminimist')
+         debug("using @rstacruz's 'rminimist' implementation") }
       catch (e) { if (e.code !== 'MODULE_NOT_FOUND') throw e
 
          try {
-            minimist = require('minimist') }
+            minimist = require('minimist')
+            debug("using @substack's 'minimist' implementation") }
          catch (e) { if (e.code !== 'MODULE_NOT_FOUND') throw e
+            print_error( multiline_error(new ArgumentError
+             , "minimist-shell: No `minimist()` package found."
+             , "`minimist-shell` must be installed alongside an implementation of `minimist`."
+             , "(Try "+chalk.inverse("`npm install rminimist`")+"!)" ))
 
-            console.error("!! minimist-shell: `minimist-shell` must be installed alongside an "+
-               "implementation of `minimist`.")
-            console.error("  (Try `npm install rminimist`!)")
-            process.exit(10)
+            process.exit(11)
    }}}
 
    argv = minimist(argv, config)
@@ -49,33 +59,41 @@ process.stdin.pipe(require('concat-stream')(function(buffer){ var config, minimi
    try {
       script = minimist_shell(argv, config) }
    catch(e) {
-      fail(e) }
+      print_error(e)
+      process.exit(12)
+   }
 
    debug("generated script, printing it to calling shell")
    console.log(script)
 }))
 
 function no_config(){
-   const e = new Error("!! minimist-shell: No config provided on standard-input.")
+   // FIXME: This example should show correct shell-sourcing behaviour
+   print_error( multiline_error(new ArgumentError
+    , "minimist-shell: No config provided on standard-input!"
+    , "Make sure to redirect the config from a file upon invocation:"
+    , chalk.inverse("                                      ")
+    , chalk.inverse("    minimist $* <stuff/config.json    ")
+    , chalk.inverse("                                      ") ))
 
-   e.lines = [e.message
-    , "   Make sure to redirect the config from a file upon invocation:"
-    , ""
-    , "      minimist $* <stuff/config.json"
-    , ""]
+   process.exit(10)
+}
 
-   fail(e)
+function multiline_error(error, ...lines){
+   error.message = lines.join("\n")
+
+   return error
 }
 
 //haha janky tho
-function fail(err){
+function print_error(err){
    const message   = err.message.split("\n")
        , stack     = err.stack.split("\n").map(line => line.trim())
 
    console.error( chalk.red(  chalk.bold('!! ') + message[0]         )  )
+   if (message.length > 1)
    console.error(             indent(message.slice(1).join("\n"), 3)    )
 
    console.error( chalk.grey( chalk.bold('!! ') + stack[0]           )  )
    console.error( chalk.grey( indent(stack.slice(1).join("\n"), 3)   )  )
-   process.exit(51)
 }
